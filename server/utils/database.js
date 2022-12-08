3ec5f4dc16e0473cf6f25098b49d78e45d99e273
Amy Lim H L <66409683+Ocean272@users.noreply.github.com>
2022-12-08 22:34:58 +0800

    Update database.js

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('dbheroku','postgres','mypassword', {
+const sequelize = new Sequelize('dbheroku','','', {
     host: 'mydatabase-1.ctgdayi97uhz.ap-southeast-1.rds.amazonaws.com',
     dialect: 'postgres',
     
